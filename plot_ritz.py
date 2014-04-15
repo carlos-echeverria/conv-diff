fb3e5c0e24c7ac8bd6707a8b0782c957666c4a47
André Gaul <gaul@web-yard.de>
2014-04-15 18:56:42 +0200

    add animation

--- plot_ritz.py
+++ plot_ritz.py
@@ -1,7 +1,10 @@
 import conv_diff
 import numpy
 import krypy
-from matplotlib import pyplot, animation
+from matplotlib import pyplot, animation, rc
+
+# use latex in matplotlib
+rc('text', usetex=True)
 
 def get_gmres_roots(H):
     n_, n = H.shape
@@ -26,30 +29,42 @@ def main():
     H = solver.H
     n_, n = H.shape
 
-    fig, axs = pyplot.subplots(ncols=2)
+    fig, axs = pyplot.subplots(ncols=2, figsize=(1920./200,1080./200), dpi=100)
+    fig.subplots_adjust(wspace=0.3)
     line_res, = axs[0].plot([], [])
     line_roots, = axs[1].plot([], [], '.')
 
     axs[0].set_yscale('log')
     axs[0].set_xlim(0, n)
     axs[0].set_ylim(numpy.min(solver.resnorms), numpy.max(solver.resnorms))
+    axs[0].set_title('GMRES residual norm')
+    axs[0].set_xlabel('GMRES iteration $i$')
+    axs[0].set_ylabel(r'$\frac{\|r_i\|}{\|b\|}$')
 
     all_roots = numpy.concatenate([
         get_gmres_roots(H[:i+1, :i]) for i in range(1, n+1)
         ])
-    axs[1].set_xlim(numpy.min(all_roots.real), numpy.max(all_roots.real))
-    axs[1].set_ylim(numpy.min(all_roots.imag), numpy.max(all_roots.imag))
+    axs[1].set_xlim(0.95*numpy.min(all_roots.real), 1.05*numpy.max(all_roots.real))
+    axs[1].set_ylim(1.05*numpy.min(all_roots.imag), 1.05*numpy.max(all_roots.imag))
     axs[1].set_xscale('log')
+    axs[1].set_title('roots of GMRES polynomial')
+    axs[1].set_xlabel('Real part')
+    axs[1].set_ylabel('Imaginary part')
 
     def animate(i):
-        line_res.set_data(list(range(0, i)), solver.resnorms[:i])
+        print(i)
+        line_res.set_data(list(range(0, i+1)), solver.resnorms[:i+1])
         roots = get_gmres_roots(H[:i+1, :i])
         line_roots.set_data(roots.real, roots.imag)
         #return line,
 
     ani = animation.FuncAnimation(fig, animate, numpy.arange(1, n),
-            interval=25)
-    pyplot.show()
+            interval=100)
+
+    #pyplot.show()
+    Writer = animation.writers['ffmpeg']
+    writer = Writer(fps=10, bitrate=4000)
+    ani.save('gmres.mp4', writer=writer, dpi=200)
 
 
 
